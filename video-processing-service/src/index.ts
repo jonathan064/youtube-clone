e5f420f4b6cd66427dd5917be95d67d781d97caa
Jonathan <jonathanu064@yahoo.com>
2025-04-13 19:33:31 -0500

    fixed bug, 360p video working

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,26 +1,31 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg";
+
 const app = express();
+app.use(express.json());
 
 app.post("/process-video", (req, res) => {
   //Get path of input video from request body
   const inputFilePath = req.body.inputFilePath;
-  const ouputFilePath = req.body.ouputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-  if (!inputFilePath || !ouputFilePath) {
-    res.status(400).send("Bad Request: Missing file paths");
+  if (!inputFilePath) {
+    res.status(400).send("Bad Request: Missing file input");
+  } else if (!outputFilePath) {
+    res.status(400).send("Bad Request: Missing file output");
   }
 
   ffmpeg(inputFilePath)
     .outputOptions("-vf", "scale=-1:360") //360p
-    .on("end", () => {
+    .on("end", function () {
+      console.log("Processing finished successfully");
       res.status(200).send("Processing finished successfully.");
     })
-    .on("error", (err) => {
+    .on("error", function (err: any) {
       console.log(`An error occurred: ${err.message}`);
       res.status(500).send(`Internal Server Error: ${err.message}`);
     })
-    .save(ouputFilePath);
+    .save(outputFilePath);
 });
 
 const port = process.env.PORT || 3000;
