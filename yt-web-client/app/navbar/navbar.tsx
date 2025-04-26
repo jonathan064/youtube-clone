09857b00e7dd9189fbe7f0be12f0a2a5225bbf8c
Jonathan <jonathanu064@yahoo.com>
2025-04-26 13:11:03 -0500

    sign in/out button works correctly according to state

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,9 +1,23 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
 import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+    //Clean up subscription on unmount
+    return () => unsubscribe();
+  });
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
@@ -14,7 +28,7 @@ export default function Navbar() {
           alt="YouTube Logo"
         ></Image>
       </Link>
-      <SignIn></SignIn>
+      <SignIn user={user}></SignIn>
     </nav>
   );
 }
--- yt-web-client/app/navbar/sign-in.tsx
+++ yt-web-client/app/navbar/sign-in.tsx
@@ -3,16 +3,26 @@
 import { Fragment } from "react";
 import { signInWithGoogle, signOut } from "../firebase/firebase";
 import styles from "./sign-in.module.css";
+import { User } from "firebase/auth";
 
-export default function SignIn() {
+interface SignInProps {
+  user: User | null;
+}
+
+export default function SignIn({ user }: SignInProps) {
   return (
     <Fragment>
-      <button className={styles.signin} onClick={signOut}>
-        Sign Out
-      </button>
-      <button className={styles.signin} onClick={signInWithGoogle}>
-        Sign In
-      </button>
+      {user ? (
+        //If user signed in
+        <button className={styles.signin} onClick={signOut}>
+          Sign Out
+        </button>
+      ) : (
+        //If user signed out
+        <button className={styles.signin} onClick={signInWithGoogle}>
+          Sign In
+        </button>
+      )}
     </Fragment>
   );
 }
