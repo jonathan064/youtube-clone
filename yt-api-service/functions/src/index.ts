00a2c94d8bd915127869af5e62ff72b56f7ecb1a
Jonathan <jonathanu064@yahoo.com>
2025-04-29 13:26:25 -0500

    Added firebase function to create a user in db after new sign in

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,10 +1,26 @@
-import { onRequest } from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+import * as functions from "firebase-functions/v1";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+initializeApp();
+
+const db = getFirestore("yt-clone-db");
+
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+    displayName: user.displayName,
+  };
+
+  // If doc doesnt exist it will create one
+  db.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
 });
