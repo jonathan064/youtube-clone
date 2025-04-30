28875363424b893c33b16c2f4351508330bd52b7
Jonathan <jonathanu064@yahoo.com>
2025-04-29 20:39:05 -0500

    added function to upload to gcp raw storage bucket

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,7 +1,9 @@
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 import * as functions from "firebase-functions/v1";
 import * as logger from "firebase-functions/logger";
+import { Storage } from "@google-cloud/storage";
+import { onCall } from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -9,7 +11,9 @@ import * as logger from "firebase-functions/logger";
 initializeApp();
 
 const db = getFirestore("yt-clone-db");
+const storage = new Storage();
 
+const rawVideoBucketName = "064-yt-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -24,3 +28,32 @@ export const createUser = functions.auth.user().onCreate((user) => {
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
 });
+
+export const generateUploadUrl = onCall(
+  { maxInstances: 1 },
+  async (request) => {
+    // Check if the user is authentication
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated."
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique filename for upload
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return { url, fileName };
+  }
+);
