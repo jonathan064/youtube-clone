7b5d2127b715a535bbbb58bbc34238505e43d5ca
Jonathan <jonathanu064@yahoo.com>
2025-05-02 19:59:09 -0500

    fetching and displaying videos works correctly

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 export default function Watch() {
+  const videoPrefix = "https://storage.googleapis.com/064-yt-processed-videos/";
+  const videoSrc = useSearchParams().get("v");
   return (
     <div>
       <h1> Watch Page</h1>
+      <video controls src={videoPrefix + videoSrc}></video>
     </div>
   );
 }
